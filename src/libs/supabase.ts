7ba468d6fcf1fe5e19468fc5bf258aa739499c8c
JoshuaArgent <argentjackjoshua@outlook.com>
2026-03-24 21:13:17 +0000

    Remove my portfolio website code
    This is going to be hosted seperately

--- src/libs/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-//Get environment variables for supabase
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-//Create supabase client using variables
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
